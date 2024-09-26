40d4a35b10919d81a95662837f18a41f9019f379
queirozime <carloshenricco@gmail.com>
2024-09-26 00:30:20 -0300

    feat: delete all drawings and minor refactorings

--- efb/ButtonExport.js
+++ efb/ButtonExport.js
@@ -1,67 +1,67 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, TextInput, Alert, StyleSheet, Modal, Text, TouchableOpacity } from 'react-native';
 import * as translate from './GeoDocs';
-import { getLocalFile, shareFile, downloadKML } from './LocalFiles';
-
-
-// colocar no export
+import { shareFile, downloadKML } from './LocalFiles';
+import { GlobalStateContext } from './Context';
 
 
 const ExportFileButton = (props) => {
-    
-    const [fileName, setFileName] = useState('');
-    const handleExport = async () => {
+
+  const { geoJson } = useContext(GlobalStateContext);
+
+  const [fileName, setFileName] = useState('');
+  const handleExport = async () => {
     if (fileName.trim() === '') {
       Alert.alert('Erro', 'O nome do arquivo não pode estar vazio!');
     } else {
       try {
-        const jsonData = JSON.stringify(props.geoJson)
+        const jsonData = JSON.stringify(geoJson)
         let kmlData = await translate.GeoJSON2KML(jsonData);
-        let fileUri = await downloadKML(kmlData, fileName + ".kml") 
+        let fileUri = await downloadKML(kmlData, fileName + ".kml")
         await shareFile(fileUri)
         props.setModalVisible(!props.modalVisible);
       }
-      catch(e){
-        console.log('err:',e)
+      catch (e) {
+        console.log('err:', e)
       }
-        setFileName('')
+      setFileName('')
     }
   };
 
   return (
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={props.modalVisible}
-        onRequestClose={() => {
-            props.setModalVisible(!props.modalVisible);
-        }}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Digite o nome do arquivo:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Nome do arquivo"
-              value={fileName}
-              onChangeText={(text) => setFileName(text)}
-            />
-            <View style={styles.modalButtons}>
-              <TouchableOpacity
-                  style={[styles.closeButton, styles.button]}
-                  onPress={() => props.setModalVisible(!props.modalVisible)}
-                >
-                <Text style={styles.textStyle}>Cancelar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.exportButton,styles.button]} onPress={async ()=>{await handleExport()}}>
-                <Text style={styles.textStyle}>Exportar</Text>
-              </TouchableOpacity>
-              
-            </View>
-            <View />
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={props.modalVisible}
+      onRequestClose={() => {
+        props.setModalVisible(!props.modalVisible);
+      }}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Digite o nome do arquivo:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome do arquivo"
+            value={fileName}
+            onChangeText={(text) => setFileName(text)}
+          />
+          <View style={styles.modalButtons}>
+            <TouchableOpacity
+              style={[styles.closeButton, styles.button]}
+              onPress={() => props.setModalVisible(!props.modalVisible)}
+            >
+              <Text style={styles.textStyle}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.exportButton, styles.button]} onPress={async () => { await handleExport() }}>
+              <Text style={styles.textStyle}>Exportar</Text>
+            </TouchableOpacity>
+
           </View>
+          <View />
         </View>
-      </Modal>
+      </View>
+    </Modal>
   );
 };
 
@@ -74,7 +74,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.5)',
   },
   modalButtons: {
-    flexDirection:"row",
+    flexDirection: "row",
     justifyContent: 'center',
     justifyContent: 'space-evenly',
     alignItems: 'center',
